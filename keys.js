d314133424e5946b04e0f33f0b6c8f319dbc9e0d
Johnny Perez <johnny.perez2@northwestern.edu>
2017-04-10 19:22:03 -0500

    first commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Nebzu3pIqoL20klfuVmz2lSpe',
+  consumer_secret: 'bDdqD7rR4UGpOA5qOJDH5Z8OrvcZky8kwfpWQoxfdkCSODf1rh',
+  access_token_key: '522311184-fRJpNxUkgv0drFdQ7XSFNB8MMFJepnKNMPEUhN5x',
+  access_token_secret: '94ahuJGRd4KjbbyoKMbPd7GbKF1mJWiqAVtHWj6bPLqnL',
+}
---
+++ liri.js
@@ -0,0 +1,48 @@
+// gives access keys file
+var keysFile = require('./keys.js');
+var request = require('request');
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var client = new Twitter(keysFile.twitterKeys);
+var inputRequest = process.argv[2];
+var inputRequest2 = process.argv[3];
+
+
+
+
+
+if (inputRequest === 'my-tweets'){
+
+	var params = {
+		screen_name: 'johnnypgreengo',
+		count: 20,
+	};
+	
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  
+  		if (!error && response.statusCode === 200) {
+  			for (i = 0; i < tweets.length; i++){
+    			console.log(tweets[i].created_at + ' , ' + tweets[i].text);
+			}
+  		}
+	});
+};
+
+if(inputRequest === 'spotify-this-song'){
+
+	var songname = process.argv[3];
+
+	spotify.search({ type: 'track', query: songname }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+	console.log(data.tracks.items[0].artists[0].name);
+
+ 	// console.log(data.tracks.items[0].artists[0].name);
+ 	console.log()
+    // Do something with 'data' 
+});
+
+
+};
